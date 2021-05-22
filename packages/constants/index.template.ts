cbbfc861ceac5e2ea11c1d65a40a6fc88d196608
d1ll0n <dillonkellar@protonmail.com>
2021-05-22 16:49:52 -0500

    Add governance proposal statuses

--- packages/constants/index.template.ts
+++ packages/constants/index.template.ts
@@ -2,60 +2,74 @@ import { Address, BigDecimal, BigInt } from '@graphprotocol/graph-ts'
 
 /** ========= Numbers & Bytes ========= */
 
-export const ADDRESS_ZERO = Address.fromString('0x0000000000000000000000000000000000000000')
+export const BIG_INT_18 = BigInt.fromI32(18)
 
-export const BIG_DECIMAL_1E6 = BigDecimal.fromString('1e6')
+export let ADDRESS_ZERO = Address.fromString('0x0000000000000000000000000000000000000000')
 
-export const BIG_DECIMAL_1E12 = BigDecimal.fromString('1e12')
+export let BIG_DECIMAL_1E6 = BigDecimal.fromString('1e6')
 
-export const BIG_DECIMAL_1E18 = BigDecimal.fromString('1e18')
+export let BIG_DECIMAL_1E12 = BigDecimal.fromString('1e12')
 
-export const BIG_DECIMAL_ZERO = BigDecimal.fromString('0')
+export let BIG_DECIMAL_1E18 = BigDecimal.fromString('1e18')
 
-export const BIG_DECIMAL_ONE = BigDecimal.fromString('1')
+export let BIG_DECIMAL_ZERO = BigDecimal.fromString('0')
 
-export const BIG_INT_ONE = BigInt.fromI32(1)
+export let BIG_DECIMAL_ONE = BigDecimal.fromString('1')
 
-export const BIG_INT_TWO = BigInt.fromI32(2)
+export let BIG_INT_ONE = BigInt.fromI32(1)
 
-export const BIG_INT_ONE_HUNDRED = BigInt.fromI32(100)
+export let BIG_INT_TWO = BigInt.fromI32(2)
 
-export const BIG_INT_ONE_DAY_SECONDS = BigInt.fromI32(86400)
+export let BIG_INT_ONE_HUNDRED = BigInt.fromI32(100)
 
-export const BIG_INT_ZERO = BigInt.fromI32(0)
+export let BIG_INT_ONE_DAY_SECONDS = BigInt.fromI32(86400)
 
-export const NULL_CALL_RESULT_VALUE = '0x0000000000000000000000000000000000000000000000000000000000000001'
+export let BIG_INT_ZERO = BigInt.fromI32(0)
+
+export let NULL_CALL_RESULT_VALUE = '0x0000000000000000000000000000000000000000000000000000000000000001'
 
 // minimum liquidity required to count towards tracked volume for pairs with small # of Lps
-export const MINIMUM_USD_THRESHOLD_NEW_PAIRS = BigDecimal.fromString('0')
+export let MINIMUM_USD_THRESHOLD_NEW_PAIRS = BigDecimal.fromString('0')
 
 // minimum liquidity for price to get tracked
-export const MINIMUM_LIQUIDITY_THRESHOLD_ETH = BigDecimal.fromString('5')
+export let MINIMUM_LIQUIDITY_THRESHOLD_ETH = BigDecimal.fromString('5')
 
 /** ========= Tokens ========= */
 
-export const REWARDS_TOKEN_ADDRESS = Address.fromString('{{ rewards_token_address }}')
+export let REWARDS_TOKEN_ADDRESS = Address.fromString('{{ rewards_token_address }}')
 
-export const WETH_ADDRESS = Address.fromString('{{ weth_address }}')
+export let WETH_ADDRESS = Address.fromString('{{ weth_address }}')
 
-export const USDT_ADDRESS = Address.fromString('{{ usdt_address }}')
+export let USDT_ADDRESS = Address.fromString('{{ usdt_address }}')
 
-export const USDC_ADDRESS = Address.fromString('{{ usdc_address }}')
+export let USDC_ADDRESS = Address.fromString('{{ usdc_address }}')
 
-export const DAI_ADDRESS = Address.fromString('{{ dai_address }}')
+export let DAI_ADDRESS = Address.fromString('{{ dai_address }}')
 
 /** ========= Factories ========= */
 
-export const UNISWAP_FACTORY_ADDRESS = Address.fromString('{{ uniswap_factory_address }}')
+export let UNISWAP_FACTORY_ADDRESS = Address.fromString('{{ uniswap_factory_address }}')
 
 /** ========= Other ========= */
 
-export const MULTI_TOKEN_STAKING_ADDRESS = Address.fromString('{{ multi_token_staking_address }}')
+export let MULTI_TOKEN_STAKING_ADDRESS = Address.fromString('{{ multi_token_staking_address }}')
 
 /** ========= Uniswap Pairs ========= */
 
-export const USDC_WETH_PAIR = Address.fromString('{{ usdc_weth_pair }}')
+export let USDC_WETH_PAIR = Address.fromString('{{ usdc_weth_pair }}')
 
-export const DAI_WETH_PAIR = Address.fromString('{{ dai_weth_pair }}')
+export let DAI_WETH_PAIR = Address.fromString('{{ dai_weth_pair }}')
 
-export const USDT_WETH_PAIR = Address.fromString('{{ usdt_weth_pair }}')
+export let USDT_WETH_PAIR = Address.fromString('{{ usdt_weth_pair }}')
+
+/** ========= Governance Status ========= */
+
+export const STATUS_PENDING = "PENDING";
+
+export const STATUS_CANCELLED = "CANCELLED";
+
+export const STATUS_EXECUTED = "EXECUTED";
+
+export const STATUS_QUEUED = "QUEUED";
+
+export const STATUS_ACTIVE = "ACTIVE";
--- packages/constants/index.ts
+++ packages/constants/index.ts
@@ -2,60 +2,74 @@ import { Address, BigDecimal, BigInt } from '@graphprotocol/graph-ts'
 
 /** ========= Numbers & Bytes ========= */
 
-export const ADDRESS_ZERO = Address.fromString('0x0000000000000000000000000000000000000000')
+export const BIG_INT_18 = BigInt.fromI32(18)
 
-export const BIG_DECIMAL_1E6 = BigDecimal.fromString('1e6')
+export let ADDRESS_ZERO = Address.fromString('0x0000000000000000000000000000000000000000')
 
-export const BIG_DECIMAL_1E12 = BigDecimal.fromString('1e12')
+export let BIG_DECIMAL_1E6 = BigDecimal.fromString('1e6')
 
-export const BIG_DECIMAL_1E18 = BigDecimal.fromString('1e18')
+export let BIG_DECIMAL_1E12 = BigDecimal.fromString('1e12')
 
-export const BIG_DECIMAL_ZERO = BigDecimal.fromString('0')
+export let BIG_DECIMAL_1E18 = BigDecimal.fromString('1e18')
 
-export const BIG_DECIMAL_ONE = BigDecimal.fromString('1')
+export let BIG_DECIMAL_ZERO = BigDecimal.fromString('0')
 
-export const BIG_INT_ONE = BigInt.fromI32(1)
+export let BIG_DECIMAL_ONE = BigDecimal.fromString('1')
 
-export const BIG_INT_TWO = BigInt.fromI32(2)
+export let BIG_INT_ONE = BigInt.fromI32(1)
 
-export const BIG_INT_ONE_HUNDRED = BigInt.fromI32(100)
+export let BIG_INT_TWO = BigInt.fromI32(2)
 
-export const BIG_INT_ONE_DAY_SECONDS = BigInt.fromI32(86400)
+export let BIG_INT_ONE_HUNDRED = BigInt.fromI32(100)
 
-export const BIG_INT_ZERO = BigInt.fromI32(0)
+export let BIG_INT_ONE_DAY_SECONDS = BigInt.fromI32(86400)
 
-export const NULL_CALL_RESULT_VALUE = '0x0000000000000000000000000000000000000000000000000000000000000001'
+export let BIG_INT_ZERO = BigInt.fromI32(0)
+
+export let NULL_CALL_RESULT_VALUE = '0x0000000000000000000000000000000000000000000000000000000000000001'
 
 // minimum liquidity required to count towards tracked volume for pairs with small # of Lps
-export const MINIMUM_USD_THRESHOLD_NEW_PAIRS = BigDecimal.fromString('0')
+export let MINIMUM_USD_THRESHOLD_NEW_PAIRS = BigDecimal.fromString('0')
 
 // minimum liquidity for price to get tracked
-export const MINIMUM_LIQUIDITY_THRESHOLD_ETH = BigDecimal.fromString('5')
+export let MINIMUM_LIQUIDITY_THRESHOLD_ETH = BigDecimal.fromString('5')
 
 /** ========= Tokens ========= */
 
-export const REWARDS_TOKEN_ADDRESS = Address.fromString('0x86772b1409b61c639eaac9ba0acfbb6e238e5f83')
+export let REWARDS_TOKEN_ADDRESS = Address.fromString('0x86772b1409b61c639eaac9ba0acfbb6e238e5f83')
 
-export const WETH_ADDRESS = Address.fromString('0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2')
+export let WETH_ADDRESS = Address.fromString('0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2')
 
-export const USDT_ADDRESS = Address.fromString('0xdac17f958d2ee523a2206206994597c13d831ec7')
+export let USDT_ADDRESS = Address.fromString('0xdac17f958d2ee523a2206206994597c13d831ec7')
 
-export const USDC_ADDRESS = Address.fromString('0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48')
+export let USDC_ADDRESS = Address.fromString('0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48')
 
-export const DAI_ADDRESS = Address.fromString('0x6b175474e89094c44da98b954eedeac495271d0f')
+export let DAI_ADDRESS = Address.fromString('0x6b175474e89094c44da98b954eedeac495271d0f')
 
 /** ========= Factories ========= */
 
-export const UNISWAP_FACTORY_ADDRESS = Address.fromString('0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f')
+export let UNISWAP_FACTORY_ADDRESS = Address.fromString('0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f')
 
 /** ========= Other ========= */
 
-export const MULTI_TOKEN_STAKING_ADDRESS = Address.fromString('')
+export let MULTI_TOKEN_STAKING_ADDRESS = Address.fromString('0xC46E0E7eCb3EfCC417f6F89b940FFAFf72556382')
 
 /** ========= Uniswap Pairs ========= */
 
-export const USDC_WETH_PAIR = Address.fromString('0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc')
+export let USDC_WETH_PAIR = Address.fromString('0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc')
 
-export const DAI_WETH_PAIR = Address.fromString('0xA478c2975Ab1Ea89e8196811F51A7B7Ade33eB11')
+export let DAI_WETH_PAIR = Address.fromString('0xA478c2975Ab1Ea89e8196811F51A7B7Ade33eB11')
 
-export const USDT_WETH_PAIR = Address.fromString('0x0d4a11d5EEaaC28EC3F61d100daF4d40471f1852')
+export let USDT_WETH_PAIR = Address.fromString('0x0d4a11d5EEaaC28EC3F61d100daF4d40471f1852')
+
+/** ========= Governance Status ========= */
+
+export const STATUS_PENDING = "PENDING";
+
+export const STATUS_CANCELLED = "CANCELLED";
+
+export const STATUS_EXECUTED = "EXECUTED";
+
+export const STATUS_QUEUED = "QUEUED";
+
+export const STATUS_ACTIVE = "ACTIVE";
